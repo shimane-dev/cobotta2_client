217814e728fb8860f8ea5c96edc8150ae36ef849
shimane-dev <202048196+shimane-dev@users.noreply.github.com>
2025-07-25 18:45:43 +0900

    Bump version to 0.0.6, add new control script, and update changelog:
    - Introduce `try_a_COBOTTA2_v0.py` for COBOTTA testing with refined motion workflows.
    - Rename and restructure sample scripts for consistency (`try_a_vXX.py` → `try_a_COBOTTA1_vXX.py`).
    - Update `CHANGELOG.md` with new version details.
    - Test new COBOTTA2 API via FastAPI integration.
    
    - fastapi_frame: v0.3.1
    - cobotta2: v0.5.16
    - cobotta2_client: v0.0.6
    
    - fastapi_frame: v0.3.1
    - cobotta2: v0.5.16
    - cobotta2_client: v0.0.6
    - cobotta1/cobotta2 のクライアントを FastAPIでテスト
    - cobotta2 の新しい API の FastAPI 経由でのテスト

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,14 @@
 # CHANGELOG
 
-## 2025.07.20, v0.0.5  nakada
+## 2025.07.25, v0.0.6  nakada
+
+- fastapi_frame: v0.3.1
+- cobotta2: v0.5.16
+- cobotta2_client: v0.0.6
+- cobotta1/cobotta2 のクライアントを FastAPIでテスト
+- cobotta2 の新しい API の FastAPI 経由でのテスト
+
+## 2025.07.25, v0.0.5  nakada
 
 - fastapi_frame: v0.3.1
 - cobotta2: v0.5.13
---
+++ src/try_a_COBOTTA2_v0.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+"""
+try_a_v02.py (version 0.0.4)
+
+frame 0.3.0
+cobotta 0.5.5
+
+Kengo NAKADA:
+https://github.com/shimane-dev, https://github.com/kengo-nakada
+kengo.nakada@mat.shimane-u.ac.jp, kengo.nakada@gmail.com
+"""
+# import pytest
+import sys
+import asyncio
+import multiprocessing
+
+import logging
+
+# httpx のログを WARNING レベル以上にする（INFO を抑制）
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+from cobotta2.config import Config
+from cobotta2.server_fastapi.clients import AsyncCobottaClient
+from cobotta2.server_fastapi.models.motion import MotionMode
+from x_logger import XLogger
+
+
+# @pytest.mark.asyncio
+async def main():
+    logger = XLogger(log_level="info", logger_name=Config.COBOTTA_CLIENT_LOGGER_NAME)
+    t2 = asyncio.create_task(c2(logger))
+    await asyncio.gather(t2)
+    # await c2(logger)
+
+
+async def c1(
+    logger: XLogger = None,
+):
+    pass
+
+
+async def c2(
+    logger: XLogger = None,
+):
+    """"""
+    Config.load_yaml("config_server2.yaml")
+    client2 = AsyncCobottaClient(config=Config, logger=logger)
+    await client2.reset_error()
+
+    try:
+        ret = await client2.take_arm()
+        if ret is None or ret is False:
+            sys.exit(-1)
+        await client2.turn_on_motor()
+
+        # await client.set_speed(100)
+
+        # move Target
+        client2.motion_mode = MotionMode.PTP
+        await client2.move("P110", speed=50)
+
+        client2.motion_mode = MotionMode.LINE
+        await client2.move("P110", speed=60)
+        await client2.move("P111", speed=60)
+        await client2.move("P112", speed=60)
+        await client2.move("P113", speed=60)
+        await client2.move("P114", speed=60)
+        await client2.move("P115", speed=60)
+        await client2.move("P116", speed=60)
+        await client2.move("P117", speed=60)
+        await client2.open_hand()
+        await client2.wait_for_complete()
+
+        await client2.move("P116", speed=60)
+        await client2.move("P115", speed=60)
+        await client2.move("P114", speed=60)
+        await client2.move("P113", speed=60)
+        await client2.move("P112", speed=60)
+        await client2.move("P111", speed=60)
+        await client2.move("P110", speed=60)
+        await client2.wait_for_complete()
+
+        # # cobotta.move("P3", bstrOpt=100)  # home
+        # cobotta.motion_mode = MotionMode.LINE
+        # cobotta.move("@E P62+(0,0,5)", bstrOpt=100)
+        # cobotta.wait_for_complete()
+        # current_pos = cobotta.get_current_pos()
+        # logger.info(f"current_position: {current_pos}")
+
+    except Exception as e:
+        logger.error(f"error: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
